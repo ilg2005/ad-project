e8dc3b75802579a5141e287f6c1e9618da351bd5
ilg2005 <igoldfarb10@gmail.com>
2021-02-27 21:07:29 +0300

    подчищен шаблон

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueRouter from 'vue-router';
 import vuetify from './plugins/vuetify';
+import router from './router/index';
 
 Vue.config.productionTip = false
-Vue.use(VueRouter);
 
 new Vue({
   vuetify,
+  router,
   render: h => h(App)
 }).$mount('#app')
