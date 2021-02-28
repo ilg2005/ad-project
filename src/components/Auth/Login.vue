a37894b0cd5f486e6f844ef82e74cf6df482dbe6
ilg2005 <igoldfarb10@gmail.com>
2021-02-28 19:08:01 +0300

    login page

--- src/components/Auth/Login.vue
+++ src/components/Auth/Login.vue
@@ -1,18 +1,37 @@
 <template>
   <v-container>
-    <v-layout row>
-      <v-flex xs12>
+    <v-card width="50%" class="mx-auto mt-5">
+      <v-card-title class="pb-5">
         <h1>Login</h1>
-      </v-flex>
-    </v-layout>
+      </v-card-title>
+      <v-card-text>
+        <v-form>
+          <v-text-field
+              label="Username"
+              prepend-icon="mdi-account-circle"
+          />
+          <v-text-field
+              :type="showPassword ? 'text' : 'password'"
+              label="Password"
+              prepend-icon="mdi-lock"
+              :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+              @click:append="showPassword = !showPassword"
+          />
+        </v-form>
+      </v-card-text>
+      <v-divider></v-divider>
+      <v-card-actions>
+        <v-btn color="info" class="v-btn--block">Login</v-btn>
+      </v-card-actions>
+    </v-card>
   </v-container>
 </template>
 
 <script>
 export default {
-  data () {
+  data() {
     return {
-
+      showPassword: false
     }
   }
 }
