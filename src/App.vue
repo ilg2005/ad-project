86d6c4fce58d131f33c32a6f114d8b2d9ebbed7d
ilg2005 <igoldfarb10@gmail.com>
2021-02-28 11:25:39 +0300

    текущее состояние

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
-  <div>
-    <h1>Hello, Vue!</h1>
-  </div>
+  <v-app>
+    <v-app-bar app></v-app-bar>
+
+    <v-main>
+        <router-view></router-view>
+    </v-main>
+
+  </v-app>
 </template>
 
 <script>
