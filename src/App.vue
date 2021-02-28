00687c03f1a366ac5b79f1721292fd0b217515c7
ilg2005 <igoldfarb10@gmail.com>
2021-02-28 13:38:08 +0300

    текущее состояние

--- src/App.vue
+++ src/App.vue
@@ -6,17 +6,15 @@
         height="48px"
         tile
     >
-      <v-toolbar dense>
+      <v-toolbar dense dark color="primary">
         <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
-        <v-toolbar-title>Title</v-toolbar-title>
+        <v-toolbar-title>Ad Application</v-toolbar-title>
 
         <v-spacer></v-spacer>
 
         <v-toolbar-items class="hidden-sm-and-down">
           <v-btn text>Link One</v-btn>
-          <v-btn text>Link Two</v-btn>
-          <v-btn text>Link Tree</v-btn>
         </v-toolbar-items>
 
       </v-toolbar>
