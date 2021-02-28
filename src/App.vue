7b8db6bef9471a6247f6640da92daef9c59267a9
ilg2005 <igoldfarb10@gmail.com>
2021-02-28 14:49:19 +0300

    links

--- src/App.vue
+++ src/App.vue
@@ -11,8 +11,32 @@
           temporary
           v-model="drawer"
       >
+        <v-list dense>
+
+          <v-list-item-group
+              v-model="selectedItem"
+              color="primary"
+          >
+            <v-list-item
+                v-for="(link, i) in links"
+                :key="i"
+                :to="link.url"
+            >
+              <v-list-item-icon>
+                <v-icon v-text="link.icon"></v-icon>
+              </v-list-item-icon>
+
+              <v-list-item-content>
+                <v-list-item-title v-text="link.text"></v-list-item-title>
+              </v-list-item-content>
+
+            </v-list-item>
+          </v-list-item-group>
+
+        </v-list>
 
       </v-navigation-drawer>
+
       <v-toolbar dense dark color="primary">
         <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
@@ -21,9 +45,13 @@
         <v-spacer></v-spacer>
 
         <v-toolbar-items class="hidden-sm-and-down">
-          <v-btn text>
-            <v-icon dark left>mdi-bug</v-icon>
-            Link One
+          <v-btn text
+                 v-for="(link, i) in links"
+                 :key="i"
+                 :to="link.url"
+          >
+            <v-icon dark left>{{link.icon}}</v-icon>
+            {{link.text}}
           </v-btn>
         </v-toolbar-items>
 
@@ -39,9 +67,17 @@
 <script>
 
 export default {
-  data () {
+  data() {
     return {
-      drawer: false
+      drawer: false,
+      selectedItem: null,
+      links: [
+        {text: 'Login', icon: 'mdi-lock', url: '/login'},
+        {text: 'Registration', icon: 'mdi-face', url: '/registration'},
+        {text: 'Orders', icon: 'mdi-bookmark-outline', url: '/orders'},
+        {text: 'New ad', icon: 'mdi-file-plus', url: '/new'},
+        {text: 'My ads', icon: 'mdi-format-list-bulleted', url: '/list'},
+      ],
     }
   }
 };
