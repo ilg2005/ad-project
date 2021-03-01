26f770929940f5c01803590040e154f534a8a148
ilg2005 <igoldfarb10@gmail.com>
2021-03-01 22:45:56 +0300

    текущее состояние

--- src/components/Auth/Registration.vue
+++ src/components/Auth/Registration.vue
@@ -16,6 +16,7 @@
           />
           <v-text-field
               label="Email"
+              type="email"
               v-model="email"
               prepend-icon="mdi-at"
               :rules="emailRules"
