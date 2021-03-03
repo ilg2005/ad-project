26a7a1135b36507d908afec0e72328dcade5fb98
ilg2005 <igoldfarb10@gmail.com>
2021-03-03 21:46:05 +0300

    текущее состояние

--- src/components/Ads/Ad.vue
+++ src/components/Ads/Ad.vue
@@ -1,51 +1,55 @@
 <template>
   <v-container>
-    <v-layout row class="mt-5">
-      <v-card
-          class="mx-auto"
-          max-width="400"
+    <v-row class="mt-5">
+      <v-col
+          xs="12"
+          sm="6"
+          offset-sm="3"
       >
-        <v-img
-            class="white--text align-end"
-            height="200px"
-            src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+        <v-card
+            class="mx-auto"
+            max-width="400"
         >
-        </v-img>
-
-        <v-card-title class="pb-0">
-          Number 10
-        </v-card-title>
-
-        <v-card-text class="text--primary">
-          <div>Whitehaven Beach</div>
-          <div>Whitsunday Island, Whitsunday Islands</div>
-        </v-card-text>
-
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn
-              color="orange"
+          <v-img
+              class="white--text align-end"
+              height="200px"
+              src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
           >
-            Edit
-          </v-btn>
+          </v-img>
 
-          <v-btn
-              color="success"
-          >
-            Buy
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-layout>
+          <v-card-title class="pb-0">
+            Number 10
+          </v-card-title>
+
+          <v-card-text class="text--primary">
+            <div>Whitehaven Beach</div>
+            <div>Whitsunday Island, Whitsunday Islands</div>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+                color="orange"
+            >
+              Edit
+            </v-btn>
+
+            <v-btn
+                color="success"
+            >
+              Buy
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
 export default {
-  data () {
-    return {
-
-    }
+  data() {
+    return {}
   }
 }
 </script>
