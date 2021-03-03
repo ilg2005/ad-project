0e9a9a7dc7b29d69a25165fded1201e0f39e99a6
ilg2005 <igoldfarb10@gmail.com>
2021-03-03 20:38:34 +0300

    Ad page

--- src/components/Ads/Ad.vue
+++ src/components/Ads/Ad.vue
@@ -1,9 +1,41 @@
 <template>
   <v-container>
-    <v-layout row>
-      <v-flex xs12>
-        <h1>Ad</h1>
-      </v-flex>
+    <v-layout row class="mt-5">
+      <v-card
+          class="mx-auto"
+          max-width="400"
+      >
+        <v-img
+            class="white--text align-end"
+            height="200px"
+            src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+        >
+        </v-img>
+
+        <v-card-title class="pb-0">
+          Number 10
+        </v-card-title>
+
+        <v-card-text class="text--primary">
+          <div>Whitehaven Beach</div>
+          <div>Whitsunday Island, Whitsunday Islands</div>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+              color="orange"
+          >
+            Edit
+          </v-btn>
+
+          <v-btn
+              color="success"
+          >
+            Buy
+          </v-btn>
+        </v-card-actions>
+      </v-card>
     </v-layout>
   </v-container>
 </template>
