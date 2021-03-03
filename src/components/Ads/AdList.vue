f9790f8cb28f9dbb249a201f35faa9234fa1d9cc
ilg2005 <igoldfarb10@gmail.com>
2021-03-03 20:11:56 +0300

    AdList page

--- src/components/Ads/AdList.vue
+++ src/components/Ads/AdList.vue
@@ -1,9 +1,63 @@
 <template>
   <v-container>
     <v-layout row>
-      <v-flex xs12>
-        <h1>AdList</h1>
-      </v-flex>
+      <v-col
+          xs="12"
+          sm="6"
+          offset-sm="3"
+      >
+        <h1 class="display-1 text--secondary my-3">My Ads</h1>
+
+          <v-container>
+            <v-row>
+              <v-col
+                  v-for="ad in ads"
+                  :key="ad.id"
+                  cols="12"
+              >
+                <v-card>
+                  <div class="d-flex flex-no-wrap justify-space-around">
+                    <v-avatar
+                        class="ma-3"
+                        size="150"
+                        tile
+                    >
+                      <v-img :src="ad.src"></v-img>
+                    </v-avatar>
+
+                    <div>
+                      <v-card-title
+                          class="headline"
+                          v-text="ad.title"
+                      ></v-card-title>
+
+                      <v-card-subtitle
+                          class="mb-5"
+                          v-text="ad.description"
+                      >
+                      </v-card-subtitle>
+
+                      <v-card-actions class="">
+                        <v-btn
+                            class="primary elevation-2 mx-auto"
+                            dark
+                            outlined
+                            small
+                            width="80px"
+                            :to="'/ad/' + ad.id"
+                        >
+                          Open
+                        </v-btn>
+                      </v-card-actions>
+                    </div>
+
+                  </div>
+                </v-card>
+              </v-col>
+            </v-row>
+          </v-container>
+
+      </v-col>
     </v-layout>
   </v-container>
 </template>
@@ -12,7 +66,36 @@
 export default {
   data () {
     return {
-
+      ads: [
+        {
+          id: '1',
+          title: 'First ad',
+          description: 'First description',
+          promo: false,
+          src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+        },
+        {
+          id: '2',
+          title: 'Second ad',
+          description: 'Second description',
+          promo: true,
+          src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+        },
+        {
+          id: '3',
+          title: 'Third ad',
+          description: 'Third description',
+          promo: true,
+          src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+        },
+        {
+          id: '4',
+          title: 'Fourth ad',
+          description: 'Fourth description',
+          promo: false,
+          src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+        },
+      ],
     }
   }
 }
