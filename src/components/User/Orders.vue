8202ad76491a39f8e7f0e9ec8e6b0d302ede8cba
ilg2005 <igoldfarb10@gmail.com>
2021-03-13 19:39:31 +0300

    текущее состояние

--- src/components/User/Orders.vue
+++ src/components/User/Orders.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container>
     <h1 class="display-1 text--secondary my-5" >Orders</h1>
-    <v-layout row>
+    <v-row v-if="!loading && orders.length !== 0">
       <v-col
           xs="12"
           sm="6"
@@ -30,10 +30,10 @@
                 </v-list-item-action>
 
                 <v-list-item-content>
-                  <v-list-item-title>{{ order.user }}</v-list-item-title>
+                  <v-list-item-title>{{ order.name }}</v-list-item-title>
                   <v-list-item-subtitle>{{ order.phone }}</v-list-item-subtitle>
                 </v-list-item-content>
-                <v-btn small class="primary" :to="'/ad/' + order.ad_id">Open</v-btn>
+                <v-btn small class="primary" :to="'/ad/' + order.adId">Open</v-btn>
 
               </template>
 
@@ -42,7 +42,25 @@
           </v-list-item-group>
         </v-list>
       </v-col>
-    </v-layout>
+    </v-row>
+    <div v-else-if="!loading && orders.length === 0">
+      <p class="text--secondary text-center mt-5">No orders yet!</p>
+    </div>
+    <div v-else>
+      <v-container>
+        <v-row>
+          <v-col class="text-center pt-15">
+            <v-progress-circular
+                :size="100"
+                :width="7"
+                color="purple"
+                indeterminate
+            ></v-progress-circular>
+          </v-col>
+        </v-row>
+      </v-container>
+    </div>
+
   </v-container>
 </template>
 
@@ -50,17 +68,21 @@
 export default {
   data () {
     return {
-      orders: [
-        {
-          id: 'order_1',
-          ad_id: '1',
-          user: 'Vladilen',
-          phone: '+7 (111) 111-11-11',
-          done: false
-        }
-      ]
+      currentUserId: ''
     }
   },
+  computed: {
+    orders () {
+      return this.$store.getters.orders
+    },
+    loading() {
+      return this.$store.getters.loading
+    }
+  },
+  created () {
+    this.currentUserId = this.$store.getters.currentUser.uid
+    this.$store.dispatch('fetchOrders', this.currentUserId)
+  }
 }
 </script>
 
--- src/store/orders.js
+++ src/store/orders.js
@@ -1,5 +1,16 @@
 import firebase from "firebase/firebase";
 
+class Order {
+    constructor(name, phone, adId, done, id = null) {
+        this.name = name
+        this.phone = phone
+        this.adId = adId
+        this.done = done
+        this.id = id
+    }
+}
+
+
 export default {
     state: {
         orders: []
@@ -7,20 +18,52 @@ export default {
     mutations: {
         createOrder (state, payload) {
             state.orders.push(payload)
-        }
+        },
+        loadOrders(state, payload) {
+            state.orders = payload
+        },
     },
     actions: {
         async createOrder({commit}, {name, phone, adId, ownerId}) {
             commit('clearError')
             try {
                 await firebase.database().ref(`users/${ownerId}`).push({name, phone, adId})
-                commit('setLoading', false)
             } catch (e) {
                 commit('setError', e.message)
                 throw e
             }
+        },
+        async fetchOrders({commit}, userId) {
+            commit('clearError')
+            commit('setLoading', true)
 
+            const resultOrders = []
+
+            try {
+                const fbVal = await firebase.database().ref(`users/${userId}`).once('value')
+                const orders = fbVal.val()
+                if (orders) {
+                    Object.keys(orders).forEach(key => {
+                        const order = orders[key]
+                        resultOrders.push(new Order(
+                            order.name,
+                            order.phone,
+                            order.adId,
+                            order.done,
+                            key
+                        ))
+                    })
+                    commit('loadOrders', resultOrders)
+                }
+                commit('setLoading', false)
+
+            } catch (e) {
+                commit('setLoading', false)
+                commit('setError', e.message)
+                throw e
+            }
         }
+
     },
     getters: {
         orders (state) {
